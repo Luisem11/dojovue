8dcbd8953fa9c65d54aced6d4c426877d7c59256
explora <luis.monsalve@parqueexplora.org>
2020-09-19 09:59:06 -0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+   <v-app>
+    <Toolbar />
+    <v-main>
+      <Search />
+      <ImageList/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Toolbar from "./components/Toolbar";
+import Search from "./components/Search";
+import ImageList from "./components/ImageList";
+
+
+export default {
+  name: 'App',
+
+  components: {
+    Toolbar,
+    Search,
+    ImageList
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
